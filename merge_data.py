126274a368ca04c73b6cb1e4f8920e94bd020d7e
adham <odinay@gmail.com>
2018-04-30 14:21:40 +1000

    add merge_data to merge data across days

--- merge_data.py
+++ merge_data.py
@@ -1,20 +1,49 @@
 import os
 import sys
+import pandas as pd
+from tqdm import tqdm
+from collections import defaultdict
+
 
 def find_files(pin, tag):
 
     file_names = os.listdir(pin)
     file_names = [ff for ff in file_names if tag in ff]
-    1/0
+    file_names = [os.path.join(pin, ff) for ff in file_names]
 
-def main(pin):
+    return file_names
+
+
+def main(pin, pout):
 
     sender_files = find_files(pin, 'sender')
-    pass
+    recipient_files = find_files(pin, 'recipient')
+
+    sender_data = defaultdict(list)
+    recipient_data = defaultdict(list)
+
+    print('===> building senders\' dictionary')
+    for file_name in tqdm(sender_files):
+        data = pd.read_pickle(file_name)
+        for k, v in data.items():
+            sender_data[k] += v
+
+    print()
+    print('===> building recipients\' dictionary')
+    for file_name in tqdm(recipient_files):
+        data = pd.read_pickle(file_name)
+        for k, v in data.items():
+            recipient_data[k] += v
+
+
+    print()
+    print('===> writing the redult on disk')
+    pd.to_pickle({'sender_data': sender_data, 'recipient_data': recipient_data}, pout)
 
 
 if __name__ == '__main__':
-    args = sys.argv()
+    args = sys.argv
 
-    data_dir = args[1]
-    main(data_dir)
+    pin = args[1]
+    pout = args[2]
+    main(pin, pout)
